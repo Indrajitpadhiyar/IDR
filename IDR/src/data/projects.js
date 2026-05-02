92ac2c098a2010fbb08bac422458103b880439ec
indrajit <indrajitpadhiyar107@gmail.com>
2026-05-02 15:55:21 +0530

    change photo

--- IDR/src/data/projects.js
+++ IDR/src/data/projects.js
@@ -45,9 +45,9 @@ export const projects = [
     height: 'aspect-[3/5]',
   },
   {
-    title: 'lemp Landing page',
+    title: 'lamp Landing page',
     category: 'Design',
-    image: '/lemp.png',
+    image: '/lamp.png',
     link: 'https://wall-lemps.onrender.com',
     description: 'a premium wall-lamp layout with clean spacing and strong visual flow.',
     color: 'bg-[#E6EE9C]',
